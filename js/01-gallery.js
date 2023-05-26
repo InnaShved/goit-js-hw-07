fbb25d719e276d09b18f7a17fc22ef4c0f29e0b0
innshv <60898106+InnaShved@users.noreply.github.com>
2023-05-26 14:49:06 +0300

    task 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+const galleryList = document.querySelector('.gallery');
+
+
+galleryItems.forEach(item => {
+    galleryList.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image"
+//       src=${item.preview}
+//       data-source=${item.original}
+//       alt=${item.description} /></a></li> `);
+});
+
+function openImage(event) {
+    event.preventDefault();
+   
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    
+    const imageLink = event.target.getAttribute('data-source');
+    console.log(imageLink);
+
+    basicLightbox.create(`
+		<img src='${imageLink}'/>
+	`).show();
+}
+
+galleryList.addEventListener('click', openImage);
+
+
+
+
+
+
+
+
